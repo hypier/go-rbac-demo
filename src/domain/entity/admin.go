005bca4318b7c27072976b4d2242b631d7f2a69d
heyong <hypier@qq.com>
2020-07-08 18:10:08 +0800

    调整代码

--- src/domain/entity/admin.go
+++ src/domain/entity/admin.go
@@ -1,5 +1,12 @@
 package entity
 
+import (
+	"bytes"
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+)
+
 type Admin struct {
 	//BaseEntity
 	AdminId       int
@@ -7,4 +14,23 @@ type Admin struct {
 	AdminPassword string
 	RoleCode      string
 	Role          *Role
+}
+
+func (r *Role) Scan(src interface{}) error {
+	b, ok := src.([]uint8)
+
+	if !ok {
+		return nil
+	}
+
+	_ = json.Unmarshal(b, &r)
+
+	return nil
+}
+
+func (r Role) Value() (driver.Value, error) {
+	buf := new(bytes.Buffer)
+	role, _ := json.Marshal(r)
+	fmt.Fprintf(buf, "%s", role)
+	return buf.Bytes(), nil
 }
--- src/domain/entity/role.go
+++ src/domain/entity/role.go
@@ -1,33 +1,7 @@
 package entity
 
-import (
-	"bytes"
-	"database/sql/driver"
-	"encoding/json"
-	"fmt"
-)
-
 type Role struct {
 	RoleId   int    `json:"role_id"`
 	RoleCode string `json:"role_code"`
 	RoleName string `json:"role_name"`
-}
-
-func (r *Role) Scan(src interface{}) error {
-	b, ok := src.([]uint8)
-
-	if !ok {
-		return nil
-	}
-
-	_ = json.Unmarshal(b, &r)
-
-	return nil
-}
-
-func (r Role) Value() (driver.Value, error) {
-	buf := new(bytes.Buffer)
-	role, _ := json.Marshal(r)
-	fmt.Fprintf(buf, "%s", role)
-	return buf.Bytes(), nil
 }
